5cabdfc763e8f96413b6db25d6dd4b986d645f82
Ag0r9 <Ag0r@protonmail.com>
2021-09-03 14:53:15 +0200

    dirty: push for code review

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import extra_streamlit_components as stx
 import pandas as pd
-from PIL import Image
+from sklearn.linear_model import LogisticRegression
 
 
 @st.cache
@@ -10,36 +10,59 @@ def load_data(data_name):
     return df
 
 
-cookie_manager = stx.CookieManager()
-data = load_data('train.csv')
-background = Image.open('assets/background.jpg')
+# @st.cache
+# def train_model(features, labels):
+#     model = LogisticRegression()
+#     model.fit(features, labels)
+#     return model
 
-st.title('What would be your chances to survive on the Titanic?')
-st.image(background, use_column_width=True)
 
-st.sidebar.subheader('Write Data')
-age = st.sidebar.slider('Enter an age', min_value=0, max_value=100, step=1,
-                        value=int(cookie_manager.get('age') if cookie_manager.get('age') else 0))
-sex = st.sidebar.radio('Sex', options=['Female', 'Male', 'waiting till marriage'])
-pclasses = {'Upper': 1, 'Middle': 2, 'Lower': 3}
-pclass_key = st.sidebar.selectbox('Select ticket class', pclasses.keys())
-pclass = pclasses[pclass_key]
-sib_sp = st.sidebar.number_input('How many siblings would you take with you?', min_value=0, max_value=20,
-                                 step=1)
-with_loved = st.sidebar.radio('Would you take a husband/wife with you?', ['Yes', 'No'])
-sib_sp += 1 if with_loved == 'Yes' else 0
-parents_answer = {'Yes': 2, 'Only mother': 1, 'Only father': 1, 'No': 0}
-parents_answer_key = st.sidebar.radio('Would you take a parents with you?', parents_answer.keys())
-parents = parents_answer[parents_answer_key]
-children = st.sidebar.number_input('How many children would you take with you?', min_value=0, max_value=10,
-                                   step=1)
-parchar = parents + children
+def main():
+    cookie_manager = stx.CookieManager()
+    # data = load_data('train.csv')
+    # x_train = data[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']]
+    # y_train = data['Survived']
+    # model = train_model(x_train, y_train)
 
-cookie_manager.set('age', age, key=11)
-cookie_manager.set('sex', sex, key=12)
-cookie_manager.set('pclass', pclass, key=13)
-cookie_manager.set('sib_sp', sib_sp, key=14)
+    sexes = ['Female', 'Male']
+    pclasses = [1, 2, 3]
 
-st.sidebar.subheader('All Cookies:')
-cookies = cookie_manager.get_all()
-st.sidebar.write(cookies)
+    st.title('What would be your chances to survive on the Titanic?')
+    st.image('assets/background.jpg', use_column_width=True)
+
+    st.sidebar.subheader('Write Data')
+    age = st.sidebar.slider('Enter an age', min_value=0, max_value=100, step=1)
+    sex = st.sidebar.radio('Sex', options=sexes)
+    pclass = st.sidebar.selectbox('Select ticket class', options=pclasses)
+    siblings = st.sidebar.number_input('How many siblings would you take with you?', min_value=0, max_value=20,
+                                       step=1)
+    with_loved = st.sidebar.radio('Would you take a husband/wife with you?', ['Yes', 'No'])
+    sib_sp = siblings + 1 if with_loved == 'Yes' else siblings
+    parents_answer = {'Yes': 2, 'Only mother': 1, 'Only father': 1, 'No': 0}
+    parents_answer_key = st.sidebar.radio('Would you take a parents with you?', parents_answer.keys())
+    parents = parents_answer[parents_answer_key]
+    children = st.sidebar.number_input('How many children would you take with you?', min_value=0, max_value=10,
+                                       step=1)
+    par_child = parents + children
+
+    # test = pd.DataFrame({'Pclass': pclass,
+    #                      'Sex': sex,
+    #                      'Age': age,
+    #                      'SibSp': sib_sp,
+    #                      'Parch': par_child})
+    # test
+    # prediction = model.predict_proba(test)
+
+    cookie_manager.set('pclass', pclass, key=10)
+    cookie_manager.set('sex', sex, key=11)
+    cookie_manager.set('age', age, key=12)
+    cookie_manager.set('sibSp', sib_sp, key=13)
+    cookie_manager.set('parch', par_child, key=14)
+
+    st.sidebar.subheader('All Cookies:')
+    cookies = cookie_manager.get_all()
+    st.sidebar.write(cookies)
+
+
+if __name__ == '__main__':
+    main()
